2d95219a23180142d00764d9f780a87f721d6186
yc5424tl <yc5424tl@go.minneapolis.edu>
2019-06-05 19:36:13 -0500

    HOW TO PUT THE CONTENTS OF client_secret.json IN TO A HEROKU ENV VAR ON WINDOWS           - In Powershell (Win7, as Win10 has a bash shell built in)
    >new-variable [name] (get-content [path-to-client_secret.json])
    
    >$[other_name] = "`"$[name]" -replace '"', "'"
    
    >heroku config:set GOOGLE_APPLICATION_CREDENTIALS="$[name]" -a [app-name]

--- manage.py
+++ manage.py
@@ -3,7 +3,7 @@ from flask_migrate import Migrate, MigrateCommand, Manager
 from vault import create_app
 
 manager = Manager(create_app)
-manager.add_option("-c", "--config", dest="config_module", required=False)
+# manager.add_option("-c", "--config", dest="config_module", required=False)
 manager.add_command('db', MigrateCommand)
 
 if __name__ == '__main__':
